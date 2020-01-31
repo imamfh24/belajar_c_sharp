5c13111e10fc31ac7f3ee86bc272310f2d28df7d
IFA <imamfh24@gmail.com>
2020-02-01 02:06:22 +0700

    membuat program mencari luas inputan statis

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        //----Program mencari Luas tanpa input----
+
+        //Deklarasi Variabel
+        int p, l, L;
+
+        //Beri Statement
+        p = 10;
+        l = 5;
+        L = p * l;
+
+        //Output
+        System.Console.WriteLine("Luas = " + L);
         Console.WriteLine("Hello world! My first program in C#");
     }
 }
