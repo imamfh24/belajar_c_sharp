129ed8180e918282a1f17d30ec0d79d835b0bab0
IFA <imamfh24@gmail.com>
2020-02-01 01:02:18 +0700

    menghapus namespace

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-
-namespace belajar_c_sharp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Hello World!");
     }
 }
