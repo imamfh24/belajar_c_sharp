546570ea38717d1ce18146f9dfe52c300d6f31bd
IFA <imamfh24@gmail.com>
2020-02-01 22:20:59 +0700

    membuat program perulangan bilangan

--- Program.cs
+++ Program.cs
@@ -3,6 +3,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world! My first program in C#");
+        //--Membuat program untuk menampilkan suatu bilangan dari 1 - bilangan yang diinputkan
+
+        //Deklarasi Variabel
+        int angka;
+
+        //Deklarasi Input
+        String input;
+
+        //--Membuat Judul Program
+        System.Console.WriteLine("--Program Perulangan Angka--");
+
+        //Input Angka
+        System.Console.Write("Masukkan Angka : ");
+        input = Console.ReadLine();
+        int.TryParse(input, out angka);
+
+        //Proses dan output
+        for (int x = 1; x <= angka; x++)
+        {
+            System.Console.Write(x + " ");
+        }
+        System.Console.WriteLine("\n--Program by Imamfh24--");
     }
 }
