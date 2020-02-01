42d222e1d39c4ccc190d9a6059059fb4fda988f1
IFA <imamfh24@gmail.com>
2020-02-02 00:33:08 +0700

    membuat program menampilkan bilangan ganjil

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //--Membuat program untuk menampilkan suatu bilangan dari 1 - bilangan yang diinputkan
+        //--Program menampilkan bilangan ganjil
 
         //Deklarasi Variabel
         int angka;
@@ -12,17 +12,21 @@ class Program
         String input;
 
         //--Membuat Judul Program
-        System.Console.WriteLine("--Program Perulangan Angka--");
+        System.Console.WriteLine("--Program Menampilkan Bilangan Ganjil--");
 
         //Input Angka
         System.Console.Write("Masukkan Angka : ");
         input = Console.ReadLine();
         int.TryParse(input, out angka);
 
+        System.Console.Write("Angka Ganjil : ");
         //Proses dan output
         for (int x = 1; x <= angka; x++)
         {
-            System.Console.Write(x + " ");
+            if (x % 2 != 0)
+            {
+                System.Console.Write(x + " ");
+            }
         }
         System.Console.WriteLine("\n--Program by Imamfh24--");
     }
