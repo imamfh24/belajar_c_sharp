559330b24db912d4679d4e23f8f58591c2496bea
IFA <imamfh24@gmail.com>
2020-02-02 19:04:33 +0700

    membuat program random angka

--- Program.cs
+++ Program.cs
@@ -3,6 +3,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world! My first program in C#");
+        //--Program Array 1 Dimensi
+
+        //Judul Program
+        System.Console.WriteLine("--Random Angka--");
+
+        //Input String
+        String input;
+
+        //Statement
+        System.Console.Write("Masukkan banyak angka yang akan dirandom : ");
+        input = Console.ReadLine();
+        int.TryParse(input, out int angka);
+
+        //Deklarasi Variabel Array
+        // Rumus deklarasi variabel array yaitu tipedata[] namavariabel = new tipedata[jumlah array];
+        int[] loker = new int[angka];
+
+        //Fungsi random angka
+        int random;
+        Random rnd = new Random();
+
+        for (int x = 0; x < loker.Length; x++)
+        {
+            random = rnd.Next(1, angka);
+            loker[x] = random;
+            System.Console.Write(loker[x] + " ");
+        }
     }
 }
